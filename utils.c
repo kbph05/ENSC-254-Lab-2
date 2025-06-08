b8cd288b0ee26f26fc22234398206d22c9786d7a
kbph05 <horvatk@shaw.ca>
2025-06-08 11:14:31 -0700

    1st version of utils.c

--- utils.c
+++ utils.c
@@ -4,8 +4,11 @@
 
 /* Unpacks the 32-bit machine code instruction given into the correct
  * type within the instruction struct */
+
+//// Kirstin ////
 Instruction parse_instruction(uint32_t instruction_bits) {
   /* YOUR CODE HERE */
+
   Instruction instruction;
   // add x9, x20, x21   hex: 01 5A 04 B3, binary = 0000 0001 0101 1010 0000 0100 1011 0011
   // Opcode: 0110011 (0x33) Get the Opcode by &ing 0x1111111, bottom 7 bits
@@ -39,6 +42,111 @@ Instruction parse_instruction(uint32_t instruction_bits) {
   // cases for other types of instructions
   /* YOUR CODE HERE */
 
+  // I-Type
+  case 0x10011: // 0x11 OR 0x1110011
+    iTypeInstructions(instruction, instruction_bits);
+    /* // 0000 0001 0101 1010 0000 0100 1
+    // instruction.itype.rd = instruction_bits;
+    // instruction_bits >>= 5;
+
+    // // 0000 0001 0101 1010 0000
+    // instruction.itype.funct3 = instruction_bits;
+    // instruction_bits >>= 3;
+
+    // // 0000 0001 0101 1010 0
+    // instruction.itype.rs1 = instruction_bits;
+    // instruction_bits >>= 5;
+
+    // // 0000 0001 0101
+    // instruction.itype.imm = instruction_bits;
+    // instruction_bits >>= 12; */
+  break;
+
+  // I-Type
+  case 0x11:
+    iTypeInstructions(instruction, instruction_bits);
+  break;
+
+  // I-Type
+  case 0x1110011:
+    iTypeInstructions(instruction, instruction_bits);
+  break;
+
+  // S-Type
+  case 0x100011:
+
+    // 0000 0001 0101 1010 0000 0100 1
+    instruction.stype.imm5 = instruction_bits;
+    instruction_bits >>= 5;
+
+    // 0000 0001 0101 1010 0000
+    instruction.stype.funct3 = instruction_bits;
+    instruction_bits >>= 3;
+
+    // 0000 0001 0101 1010 0
+    instruction.stype.rs1 = instruction_bits;
+    instruction_bits >>= 5;
+
+    // 0000 0001 0101
+    instruction.stype.rs2 = instruction_bits;
+    instruction_bits >>= 5;
+
+    // 0000 000
+
+    instruction.stype.imm7 = instruction_bits;
+    
+  break;
+
+
+  // SB-Type
+  case 0x1100011:
+
+    // 0000 0001 0101 1010 0000 0100 1
+    instruction.sbtype.imm5 = instruction_bits;
+    instruction_bits >>= 5;
+
+    // 0000 0001 0101 1010 0000
+    instruction.sbtype.funct3 = instruction_bits;
+    instruction_bits >>= 3;
+
+    // 0000 0001 0101 1010 0
+    instruction.sbtype.rs1 = instruction_bits;
+    instruction_bits >>= 5;
+
+    // 0000 0001 0101
+    instruction.sbtype.rs2 = instruction_bits;
+    instruction_bits >>= 5;
+
+    // 0000 000
+    instruction.sbtype.imm7 = instruction_bits;
+
+  break;
+
+  // U-Type
+  case 0x110111:
+
+    //0000 0001 0101 1010 0000 0100 1
+    instruction.utype.rd = instruction_bits;
+    instruction_bits >>= 5;
+
+    // 0000 0001 0101 1010 0000
+    instruction.utype.imm = instruction_bits;
+
+  break;
+
+  // UJ-Type
+  case 0x110011:
+
+    // 0000 0001 0101 1010 0000 0100 1
+    instruction.ujtype.rd = instruction_bits;
+    instruction_bits >>= 5;
+
+    // 0000 0001 0101 1010 0000
+    instruction.ujtype.imm = instruction_bits;
+  
+  break;
+
+
   #ifndef TESTING
   default:
     exit(EXIT_FAILURE);
@@ -51,6 +159,26 @@ Instruction parse_instruction(uint32_t instruction_bits) {
 /* Here, you will need to implement a few common helper functions, 
  * which you will call in other functions when parsing, printing, 
  * or executing the instructions. */
+
+//// Helper Function which sets instructions for all the different opcodes of I-type instructions: ////
+void iTypeInstructions(Instruction instruction, uint32_t instruction_bits) {
+
+    // 0000 0001 0101 1010 0000 0100 1
+    instruction.itype.rd = instruction_bits;
+    instruction_bits >>= 5;
+
+    // 0000 0001 0101 1010 0000
+    instruction.itype.funct3 = instruction_bits;
+    instruction_bits >>= 3;
+
+    // 0000 0001 0101 1010 0
+    instruction.itype.rs1 = instruction_bits;
+    instruction_bits >>= 5;
+
+    // 0000 0001 0101
+    instruction.itype.imm = instruction_bits;
+    instruction_bits >>= 12;
+}
 
 /* Sign extends the given field to a 32-bit integer where field is
  * interpreted an n-bit integer. */
